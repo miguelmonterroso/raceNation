5dd595325fb9e772ffd91498e5ca226d0af9f238
miguelmonterroso <miguelmonterroso39@gmail.com>
2024-11-04 11:43:50 -0600

    testing photo gallery

--- app/ranking/[category]/[eventId]/page.tsx
+++ app/ranking/[category]/[eventId]/page.tsx
@@ -1,27 +1,115 @@
 // /rankings/[category]/[eventId]/page.tsx
 'use client';
 
+import { useState, useEffect } from 'react';
 import { eventData } from '../../data/data';
 import Multi from '@/components/slider/multi/multi';
 import RankingDataTable from '@/components/rankingTable/rankingTable';
+import Image from 'next/image';
+import BlurFade from '@/components/ui/blur-fade';
+
+// Importa el Lightbox y el plugin de descarga
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
+import Download from 'yet-another-react-lightbox/plugins/download';
+import { Button } from '@/components/ui/button';
 
 export default function EventDetailPage({ params }: { params: { category: string; eventId: string } }) {
   const { category, eventId } = params;
 
-  const event = eventData[category as keyof typeof eventData]?.find(e => e.id === Number(eventId));
+  const event = eventData[category as keyof typeof eventData]?.find(e => e.url === eventId);
   
+  const [showFullDescription, setShowFullDescription] = useState(false);
+  const [charLimit, setCharLimit] = useState(120);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false); // Estado para manejar la apertura del lightbox
+
+  useEffect(() => {
+    const updateCharLimit = () => {
+      if (window.matchMedia('(min-width: 1024px)').matches) {
+        setCharLimit(1000); 
+      } else {
+        setCharLimit(120); 
+      }
+    };
+
+    updateCharLimit(); 
+    window.addEventListener('resize', updateCharLimit); 
+
+    return () => window.removeEventListener('resize', updateCharLimit);
+  }, []);
+
   if (!event) return <p className='text-5xl p-12 font-bold'>Evento no encontrado</p>;
 
+  const toggleDescription = () => {
+    setIsAnimating(true);
+    setShowFullDescription(prevState => {
+      if (prevState) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+      return !prevState;
+    });
+  };
+
+  // Configuración de imágenes para el lightbox
+  const images = event.images.map((img: string) => ({
+    src: img,
+    downloadUrl: img, // URL de la imagen para que sea descargable
+  }));
+
   return (
-    <div className="p-12 mt-10">
-      <h1 className="font-bold text-5xl mb-4">{event.title} - {event.date}</h1>
-      <p className="text-lg text-gray-400 mb-2">{event.description}</p>
-      <p className="text-gray-500 mb-4">Fecha: {event.date}</p>
-      <h2 className='text-4xl font-bold mt-10 mb-10'>Aqui te dejamos los resultados de la fecha:</h2>
-      {/* <Image src={event.image} alt={event.title} width={500} height={500}/> */}
-      <RankingDataTable/>
-      <Multi/>
-      
-    </div>
+    <BlurFade delay={0.25} inView>
+      <div className="mt-10">
+        <h1 className="font-bold text-5xl mb-4 pl-12 pr-12">{event.title} - {event.date}</h1>
+        <div className='flex flex-col lg:flex-row pl-12 pr-12'>
+          <div className='pr-20'>
+            <p className="text-lg text-gray-400 mb-2 mt-2">
+              {event.description.length <= charLimit ? (
+                event.description
+              ) : (
+                <>
+                  {showFullDescription ? (
+                    <BlurFade delay={0.2} inView={isAnimating}>
+                      {event.description}
+                    </BlurFade>
+                  ) : (
+                    `${event.description.slice(0, charLimit)}... `
+                  )}
+                  <span
+                    className="text-blue-500 cursor-pointer"
+                    onClick={toggleDescription}
+                  >
+                    {showFullDescription ? " Ver menos" : " Ver más"}
+                  </span>
+                </>
+              )}
+            </p>
+            <p className="text-gray-500 mb-4">Fecha: {event.date}</p>
+          </div>
+          <Image src={event.image} alt={event.title} width={500} height={500} className='max-h-80'/>
+        </div>
+        <h2 className='text-4xl font-bold mt-10 mb-10 pl-12 pr-12'>Aquí te dejamos los resultados de la fecha:</h2>
+        <RankingDataTable/>
+
+        {/* Botón para abrir el lightbox */}
+        <div className="text-center mt-10 mb-10">
+          <Button onClick={() => setIsLightboxOpen(true)}>
+            Ver fotos del evento
+          </Button>
+
+        </div>
+
+        {isLightboxOpen && (
+          <Lightbox
+            open={isLightboxOpen}
+            close={() => setIsLightboxOpen(false)}
+            slides={images}
+            plugins={[Download]}
+          />
+        )}
+
+        <Multi/>
+      </div>
+    </BlurFade>
   );
 }
--- app/ranking/[category]/page.tsx
+++ app/ranking/[category]/page.tsx
@@ -6,14 +6,14 @@ import BlurFade from '@/components/ui/blur-fade';
 
 const eventData = {
   topSpeed: [
-    { id: 1, title: "Top 10 MotorSport", description: "High-speed drag racing", date: "2024-12-01", link: "/ranking/topSpeed/1", image: "https://images.unsplash.com/photo-1693762462997-e33980ab0cd4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-    { id: 2, title: "Guatemala RaceWay", description: "Jornada 1", date: "2024-12-15", link: "/ranking/topSpeed/2", image: "https://images.unsplash.com/photo-1689827022229-f84c24d8c61d?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-    { id: 3, title: "Jocotillo Racing", description: "Etapa 3", date: "2024-12-15", link: "/ranking/topSpeed/3", image: "https://images.unsplash.com/photo-1682428609721-af4d46215283?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-    { id: 4, title: "Clandestinas", description: "Segunda edicion", date: "2024-12-15", link: "/ranking/topSpeed/4", image: "https://images.unsplash.com/photo-1592853625511-ad0edcc69c07?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { id: 1, title: "Top 10 MotorSport", description: "High-speed drag racing", date: "2024-12-01", link: "/ranking/topSpeed/top-10", image: "https://images.unsplash.com/photo-1693762462997-e33980ab0cd4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { id: 2, title: "Guatemala RaceWay", description: "Jornada 1", date: "2024-12-15", link: "/ranking/topSpeed/raceway-etapa-3", image: "https://images.unsplash.com/photo-1689827022229-f84c24d8c61d?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { id: 3, title: "Jocotillo Racing", description: "Etapa 3", date: "2024-12-15", link: "/ranking/topSpeed/jocotillo", image: "https://images.unsplash.com/photo-1682428609721-af4d46215283?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { id: 4, title: "Clandestinas", description: "Segunda edicion", date: "2024-12-15", link: "/ranking/topSpeed/clandestinas", image: "https://images.unsplash.com/photo-1592853625511-ad0edcc69c07?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
   ],
   lapTime: [
-    { id: 1, title: "Autodromo los volcanes", description: "Etapa 5", date: "2024-11-10", link: "/ranking/lapTime/1", image: "https://images.unsplash.com/photo-1624457500204-a47bebfdb725?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-    { id: 2, title: "Autodromo los volcanes", description: "Etapa 4", date: "2024-11-20", link: "/ranking/lapTime/2", image: "https://images.unsplash.com/photo-1696092516075-848e47759e7d?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { id: 1, title: "Autodromo los volcanes", description: "Etapa 5", date: "2024-11-10", link: "/ranking/lapTime/volcanes-etapa-5", image: "https://images.unsplash.com/photo-1624457500204-a47bebfdb725?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { id: 2, title: "Autodromo los volcanes", description: "Etapa 4", date: "2024-11-20", link: "/ranking/lapTime/volcanes-etapa-4", image: "https://images.unsplash.com/photo-1696092516075-848e47759e7d?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
   ]
 };
 
