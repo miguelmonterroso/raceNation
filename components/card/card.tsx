18c6b5cfd4f5c557fb6faea51305addde85de63f
miguelmonterroso <miguelmonterroso39@gmail.com>
2024-11-11 19:23:32 -0600

    problem with date solved and working fine

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -11,40 +11,48 @@ interface CardProps {
 }
 
 export default function Card({ title, date, link, image }: CardProps) {
-  const dateObj = new Date(date || "1970-01-01");
+  const dateObj = date ? new Date(date) : null;
 
-  const hours = dateObj.getUTCHours();
-  const minutes = dateObj.getUTCMinutes().toString().padStart(2, '0'); // Agrega un "0" si es necesario
-  const ampm = hours >= 12 ? 'PM' : 'AM';
-  const formattedTime = `${hours % 12 || 12}:${minutes} ${ampm}`;
+  const formattedTime = dateObj
+    ? `${dateObj.getUTCHours() % 12 || 12}:${dateObj.getUTCMinutes()
+        .toString()
+        .padStart(2, '0')} ${dateObj.getUTCHours() >= 12 ? 'PM' : 'AM'}`
+    : null;
 
-  const day = dateObj.toLocaleDateString("es-ES", { weekday: "long", timeZone: 'UTC' });
-  const dayCapitalized = day.charAt(0).toUpperCase() + day.slice(1);
+  const dayCapitalized = dateObj
+    ? dateObj.toLocaleDateString("es-ES", { weekday: "long", timeZone: 'UTC' }).charAt(0).toUpperCase() +
+      dateObj.toLocaleDateString("es-ES", { weekday: "long", timeZone: 'UTC' }).slice(1)
+    : null;
 
-  const formattedDate = dateObj.toLocaleDateString("es-ES", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    timeZone: 'UTC'
-  });
+  const formattedDate = dateObj
+    ? dateObj.toLocaleDateString("es-ES", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        timeZone: 'UTC'
+      })
+    : null;
 
   return (
     <Link href={link}>
       <MagicCard className="cursor-pointer flex-col items-center justify-center shadow-2xl whitespace-nowrap text-4xl h-72 w-full relative min-h-56 max-h-56 lg:max-h-0">
         <div className="absolute w-full h-full flex justify-center flex-col opacity-100 p-10">
-            <h3 className="text-lg lg:text-2xl font-bold mb-2">{title}</h3>
-            <p className="text-xs mt-2">{`${dayCapitalized}, ${formattedDate} - ${formattedTime}`}</p>
+          <h3 className="text-lg lg:text-2xl font-bold mb-2">{title}</h3>
+          {date && (
+            <p className="text-xs mt-2">
+              {`${dayCapitalized}, ${formattedDate} - ${formattedTime}`}
+            </p>
+          )}
         </div>
         <Image
           src={image}
           width={520}
           height={520}
-          objectFit='cover'
+          objectFit="cover"
           priority
           className="rounded-md transition-all duration-300 opacity-30"
           alt="image"
         />
-       
       </MagicCard>
     </Link>
   );
